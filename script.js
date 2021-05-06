21e5f48d1800426e24e9e9e514597184ddf4527a
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-06 12:35:27 +0545

    Learnt about create insert delete dom elements and added a cookie message button

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function (e) {
+const openModal = function(e) {
   e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = function() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -24,7 +24,7 @@ btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function(e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
@@ -33,11 +33,34 @@ document.addEventListener('keydown', function (e) {
 
 // ///////////////////////////////////
 // lectures
+// selecting elements
 console.log(document.documentElement);
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class ="btn btn--close-cookie"> Got it! </button>'
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+header.after(message);
+
+// delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  // new way
+  message.remove();
+  // old way of doing
+  // message.parentElement.removeChild(message);
+});
