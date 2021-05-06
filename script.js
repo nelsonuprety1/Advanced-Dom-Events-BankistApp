77aad07895900e90848f05e57dd4619e8b3aeaa4
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-06 18:49:41 +0545

    Styles, attrubutes and classes section learnt

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function(e) {
+const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
-const closeModal = function() {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
-
 
 // ///////////////////////////////////
 // lectures
 // selecting elements
 console.log(document.documentElement);
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
 console.log(allSections);
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
 console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+console.log(document.getElementsByClassName("btn"));
 
 // creating and inserting elements
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+const message = document.createElement("div");
+message.classList.add("cookie-message");
 // message.textContent = 'We use cookies for improved functionality and analytics';
-message.innerHTML = 'We use cookies for improved functionality and analytics. <button class ="btn btn--close-cookie"> Got it! </button>'
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class ="btn btn--close-cookie"> Got it! </button>';
 
 // header.prepend(message);
 header.append(message);
@@ -58,9 +58,51 @@ header.append(message);
 header.after(message);
 
 // delete elements
-document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-  // new way
-  message.remove();
-  // old way of doing
-  // message.parentElement.removeChild(message);
-});
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // new way
+    message.remove();
+    // old way of doing
+    // message.parentElement.removeChild(message);
+  });
+
+// styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// atributes
+const logo = document.querySelector('.nav__logo');
+
+
+logo.alt = 'Beautiful minimalist logo';
+// Non standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// data attributes
+console.log(logo.dataset.versionNumber);
+
+// classes
+logo.classList.add('c','g');
+logo.classList.remove('c','s');
+logo.classList.toggle('c','s');
+logo.classList.contains('c','s');
+
+// dont use
+// logo.className = 'nelson'
