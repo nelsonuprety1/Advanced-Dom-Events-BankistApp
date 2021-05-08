30a149006c216a3bd24385dd6f2444bb29f60e86
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-08 08:51:42 +0545

    added tabbed components. adding and removing class on the click of the button

--- script.js
+++ script.js
@@ -87,8 +87,29 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab');
+  
+  // Guard clause
+  if(!clicked) return;
+  
+  // remove active classes for both the tab and content areas
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  
+  // activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+  .classList.add('operations__content--active');
+});
 
 
 // ///////////////////////////////////
 // lectures
-// selecting elements
