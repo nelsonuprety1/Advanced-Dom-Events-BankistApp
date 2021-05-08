3fef6b5baf1260feee60da3d24b13b6f542a6ad3
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-08 09:57:44 +0545

    modified some changes

--- script.js
+++ script.js
@@ -132,6 +132,21 @@ nav.addEventListener('mouseover',handleHover.bind(0.5));
 
 nav.addEventListener('mouseout',handleHover.bind(1));
 
+// Sticky navigation
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener('scroll',function(e){
+//   console.log(window.scrollY);
+
+//   if(this.window.scrollY > initialCoords.top){
+//     nav.classList.add('sticky');
+//   }else{
+//     nav.classList.remove('sticky');
+//   }
+// });
+
+// Sticky navigation: Intersection Observer API
 
 // ///////////////////////////////////
 // lectures
