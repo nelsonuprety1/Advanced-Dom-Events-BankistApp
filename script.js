920dabbc2f262a9eaa6bb682649bed89143edffd
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-08 09:27:38 +0545

    learnt about passing arguments to event handlers

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -88,9 +92,7 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
 });
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+
 
 tabsContainer.addEventListener('click', function(e){
   const clicked = e.target.closest('.operations__tab');
@@ -109,6 +111,26 @@ tabsContainer.addEventListener('click', function(e){
   document.querySelector(`.operations__content--${clicked.dataset.tab}`)
   .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function(e){
+   if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').
+    querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// passing an 'argument' into handler
+nav.addEventListener('mouseover',handleHover.bind(0.5));
+
+nav.addEventListener('mouseout',handleHover.bind(1));
 
 
 // ///////////////////////////////////
